52894c03699801540449803bf0dbf9b653853877
NED <NED@NED-PC.fritz.box>
2012-05-01 21:26:59 +0200

    there is no way to make it through without asking about the graph type :( i have changed the neighbours vertex method,
    TODO, 
    i need your help in asking about the graph type .. and to compare result with the orignal Dijkstra algorithm

--- JavaApplication2/src/start/Dijkstra.java
+++ JavaApplication2/src/start/Dijkstra.java
@@ -4,10 +4,145 @@
  */
 package start;
 
+import java.util.*;
+import org.jgrapht.Graph;
+
 /**
  *
  * @author Tobi
  */
-public class Dijkstra<V, E> {
+public class Dijkstra<V extends Comparable<? super V>, E> {
     
+    Graph<V, E> myGraph;
+    private final static boolean LOG = false;
+    List<V> vertexList;
+    Map<V, Double> minDistance = new HashMap();
+    // Vorgänger Vertex nach Dijkstra algo.
+    Map<V, V> previous = new HashMap();
+    
+    public Dijkstra(Graph<V, E> myGraph) {
+        this.myGraph = myGraph;
+        vertexList = new ArrayList(myGraph.vertexSet());
+        // Die Entfernung am Anfang auf unendlich setzen
+        for (V vertex : vertexList) {
+            minDistance.put(vertex, Double.MAX_VALUE);
+        }
+        computePaths(vertexList.get(0));
+        for (V v : vertexList) {
+            System.out.println("Distance to " + v + ": " + minDistance.get(v));
+            List<V> path = getShortestPathTo(v);
+            System.out.println("Path: " + path);
+        }
+    }
+    
+    public void computePaths(V source) {
+        minDistance.put(source, 0.0);
+        PriorityQueue<V> vertexQueue = new PriorityQueue<V>();
+        vertexQueue.add(source);
+        
+        while (!vertexQueue.isEmpty()) {
+            V u = vertexQueue.poll();
+            if (myGraph== "undirected") {
+                // Visit each edge exiting u
+                
+                List<V> neigbors = getAllNeigbors(myGraph, u);
+                for (V e :
+                        neigbors) {
+                    double weight =
+                            myGraph.getEdgeWeight(myGraph.getEdge(u, e));
+                    if (weight < 0) {
+                        System.out.println("Negative Kanten Gewicht sind nicht ünterstützt in Dijkstra Algorithmus");                        
+                        minDistance.clear();
+                        vertexList.clear();
+                        return;
+                    }
+                    double distanceThroughU =
+                            minDistance.get(u) + weight;
+                    if (distanceThroughU
+                            < minDistance.get(e)) {
+                        vertexQueue.remove(e);
+                        minDistance.put(e,
+                                distanceThroughU);
+                        previous.put(e, u);
+                        vertexQueue.add(e);
+                    }
+                }
+            } else {
+                for (E e : myGraph.edgesOf(u)) {
+                    V v = myGraph.getEdgeTarget(e);
+//                if (v.equals(u)){
+//                    v = myGraph.getEdgeSource(e);
+//                }
+                    double weight = myGraph.getEdgeWeight(e);
+                    if (weight < 0) {
+                        System.out.println("Negative Kanten Gewicht sind nicht ünterstützt in Dijkstra Algorithmus");
+                        minDistance.clear();
+                        vertexList.clear();
+                        return;
+                    }
+                    
+                    double distanceThroughU = minDistance.get(u) + weight;
+                    
+                    if (distanceThroughU < minDistance.get(v)) {
+                        vertexQueue.remove(v);
+                        minDistance.put(v, distanceThroughU);
+                        previous.put(v, u);
+                        vertexQueue.add(v);
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    public List<V> getShortestPathTo(V target) {
+        List<V> path = new ArrayList<V>();
+        for (V vertex = target; vertex != null; vertex = previous.get(vertex)) {
+            path.add(vertex);
+        }
+        Collections.reverse(path);
+        return path;
+    }
+    
+    private List<V> getAllNeigbors(Graph<V, E> g, V v) {
+        if (LOG) {
+            System.out.println("#########getAllNeigbors-START" + v);
+        }
+
+        //Set, welches alle Nachbarn enthalten soll
+        List<V> result = new ArrayList<>();
+
+        //Set mit den angrenzenden Kanten der zu bearbeitenden Ecke
+        Set<E> edgesOfMainVertex = g.edgesOf(v);
+        
+        for (E edge : edgesOfMainVertex) {
+            
+            if (g.getEdgeTarget(edge).equals(v)) {
+                result.add(g.getEdgeSource(edge));
+            } else {
+                result.add(g.getEdgeTarget(edge));
+            }
+        }
+
+        /*
+         * // Nun wird über alle Ecken des Graphs traversiert for (V
+         * otherVertex : g.vertexSet()) { //Von jeder Ecke werden die
+         * angrenzenden Kanten gesammelt Set<E> edgesOfOtherVertex =
+         * g.edgesOf(otherVertex); //Wenn beide Ecken gemeinsame Kanten haben,
+         * sind diese Ecken Nachbarn. // Deshalb wird die aktuelle Ecke zum Set
+         * der Nachbarn hinzugefügt for (E edge : edgesOfMainVertex) {
+         *
+         * if (edgesOfOtherVertex.contains(edge)) { result.add(otherVertex); } }
+         * }
+         */
+        //Eine Ecke ist nicht mit sich selbst benachbart und wird deshalb entfernt
+        result.remove(v);
+        if (LOG) {
+            System.out.println(v + " hat folgende Nachbarn: " + result);
+        }
+        if (LOG) {
+            System.out.println("#########getAllNeigbors-End");
+        }
+        return result;
+    }
 }
--- JavaApplication2/src/start/Start.java
+++ JavaApplication2/src/start/Start.java
@@ -6,17 +6,15 @@
  */
 package start;
 
-
 import org.jgrapht.Graph;
 import org.jgrapht.alg.FloydWarshallShortestPaths;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import utils.GraphParser;
 
-
 public class Start {
 
     public static void main(String[] args) {
-        
+
         //Graphen
         Graph<String, DefaultWeightedEdge> p1_undirected_cities = GraphParser.parse("src/graphFiles/graph_01.graph");
         Graph<String, DefaultWeightedEdge> p2_undirected_cities = GraphParser.parse("src/graphFiles/graph_02.graph");
@@ -25,44 +23,74 @@ public class Start {
         Graph<String, DefaultWeightedEdge> p2_undirected_negCircle_2components = GraphParser.parse("src/graphFiles/graph_05.graph");
         Graph<String, DefaultWeightedEdge> p2_directed_negEdges = GraphParser.parse("src/graphFiles/graph_06.graph");
         Graph<String, DefaultWeightedEdge> p2_directed_negCircle = GraphParser.parse("src/graphFiles/graph_07.graph");
-      
-        
+
+
         //Praktikum 1
 //        System.out.println(new Breitensuche(p1_undirected_cities));
 //        System.out.println(new Tiefensuche(p1_undirected_cities));
-        
-        //Praktikum 2
-        FloydWarshall fW01= new FloydWarshall(p1_undirected_cities); 
-        FloydWarshall fW02= new FloydWarshall(p2_undirected_cities);
-        FloydWarshall fW03= new FloydWarshall(p2_directed);
-//        FloydWarshall fW04= new FloydWarshall(p2_undirected_negCircle); //Error, negativer Kreis
-//        FloydWarshall fW05= new FloydWarshall(p2_undirected_negCircle_2components); //Error, negativer Kreis
-        FloydWarshall fW06= new FloydWarshall(p2_directed_negEdges);
-//        FloydWarshall fW07= new FloydWarshall(p2_directed_negCircle); //Error, negativer Kreis
-        
-//        fW03.getShortestDistance("a", "b"); //Error, a existier nicht
-//        fW03.getShortestDistance("s", "b"); //Error, b existier nicht
-        
-        FloydWarshallShortestPaths buildInFW = new FloydWarshallShortestPaths(p2_undirected_cities);
-        
-        System.out.println(fW02.getShortestPath("Kaiserslautern", "Rostock"));
-        System.out.println(buildInFW.getShortestPath("Kaiserslautern", "Rostock"));
-        
-        System.out.println(fW02.getShortestPath("Augsburg", "Hamburg"));
-        System.out.println(buildInFW.getShortestPath("Augsburg", "Hamburg"));
-        
-        System.out.println(fW02.getShortestPath("Karlsruhe", "Nürnberg"));
-        System.out.println(buildInFW.getShortestPath("Karlsruhe", "Nürnberg"));
 
-        
-        FloydWarshallShortestPaths buildInFW6 = new FloydWarshallShortestPaths(p2_directed_negEdges);
-        System.out.println(fW06.getShortestPath("v1", "v9"));
-        System.out.println(buildInFW6.getShortestPath("v1", "v9"));
-        
-     
-        
-        
-        
+        //Praktikum 2
+
+        System.out.println("undirected_cities");
+        System.out.println(new Dijkstra(p1_undirected_cities));
+        System.out.println();
+        System.out.println("undirected_cities");
+        System.out.println(new Dijkstra(p2_undirected_cities));
+        System.out.println();
+        System.out.println("directed");
+        System.out.println(new Dijkstra(p2_directed));
+        System.out.println();
+        System.out.println("undirected_negCircle");
+        System.out.println(new Dijkstra(p2_undirected_negCircle));
+        System.out.println();
+        System.out.println("undirected_negCircle_2components");
+        System.out.println(new Dijkstra(p2_undirected_negCircle_2components));
+        System.out.println();
+        System.out.println("directed_negEdges");
+        System.out.println(new Dijkstra(p2_directed_negEdges));
+        System.out.println();
+        System.out.println("directed_negCircle");
+        System.out.println(new Dijkstra(p2_directed_negCircle));
+
+        /*
+         * FloydWarshall fW01= new FloydWarshall(p1_undirected_cities);
+         * FloydWarshall fW02= new FloydWarshall(p2_undirected_cities);
+         * FloydWarshall fW03= new FloydWarshall(p2_directed); // FloydWarshall
+         * fW04= new FloydWarshall(p2_undirected_negCircle); //Error, negativer
+         * Kreis // FloydWarshall fW05= new
+         * FloydWarshall(p2_undirected_negCircle_2components); //Error,
+         * negativer Kreis FloydWarshall fW06= new
+         * FloydWarshall(p2_directed_negEdges); // FloydWarshall fW07= new
+         * FloydWarshall(p2_directed_negCircle); //Error, negativer Kreis
+         *
+         * // fW03.getShortestDistance("a", "b"); //Error, a existier nicht //
+         * fW03.getShortestDistance("s", "b"); //Error, b existier nicht
+         *
+         * FloydWarshallShortestPaths buildInFW = new
+         * FloydWarshallShortestPaths(p2_undirected_cities);
+         *
+         * System.out.println(fW02.getShortestPath("Kaiserslautern",
+         * "Rostock"));
+         * System.out.println(buildInFW.getShortestPath("Kaiserslautern",
+         * "Rostock"));
+         *
+         * System.out.println(fW02.getShortestPath("Augsburg", "Hamburg"));
+         * System.out.println(buildInFW.getShortestPath("Augsburg", "Hamburg"));
+         *
+         * System.out.println(fW02.getShortestPath("Karlsruhe", "Nürnberg"));
+         * System.out.println(buildInFW.getShortestPath("Karlsruhe",
+         * "Nürnberg"));
+         *
+         *
+         * FloydWarshallShortestPaths buildInFW6 = new
+         * FloydWarshallShortestPaths(p2_directed_negEdges);
+         * System.out.println(fW06.getShortestPath("v1", "v9"));
+         * System.out.println(buildInFW6.getShortestPath("v1", "v9"));
+         *
+         *
+         *
+         */
+
         //Sandbox
 
     }
